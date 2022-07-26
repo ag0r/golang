cab8e0a7a79b26476c970ab7e77718418fe0582e
ag0r <ag0r@users.noreply.github.com>
2022-07-26 11:41:27 -0500

    A simple pub/sub example

--- setup.sh
+++ setup.sh
@@ -3,3 +3,4 @@
 curl -sLO https://go.dev/dl/go1.18.4.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go && sudo tar -xzf go1.18.4.linux-amd64.tar.gz -C /usr/local
 echo "export PATH=$PATH:/usr/local/go/bin" >> /home/vagrant/.bashrc
+go get github.com/rabbitmq/amqp091-go
